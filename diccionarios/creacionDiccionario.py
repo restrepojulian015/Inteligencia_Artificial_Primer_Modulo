36fa2257d8e8752f65a4412638e73de091f6b68d
restrepojulian015 <restrepojulian015@gmail.com>
2025-08-05 11:26:53 -0500

    Creación de ciccionarios

---
+++ diccionarios/creacionDiccionario.py
@@ -0,0 +1,10 @@
+#Crear un diccionario
+
+estudiante = {
+    "nombre" : "Julian",
+    "edad" : 20,
+    "curso" : "Ingeniería de Software",
+    "activa" : True
+}
+
+print(estudiante)
---
+++ diccionarios/usoDeDict.py
@@ -0,0 +1,12 @@
+#Uso de Dict()
+
+from time import process_time_ns
+
+
+profesiones = dict([
+    ('Docto', 'Medicina'),
+    ('Ingeniero', 'Contrucción'),
+    ('Artista', 'Arte')
+])
+
+print(profesiones)
