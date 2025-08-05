56aba419c65040a7d619e345267c2e7fe6f49cd4
restrepojulian015 <restrepojulian015@gmail.com>
2025-08-05 11:30:17 -0500

    Acceso a Valores

---
+++ diccionarios/accederAlosValores.py
@@ -0,0 +1,14 @@
+#Acceder a los valores
+
+#Creación del diccionario
+
+producto = {
+    "nombre" : "Laptop",
+    "precio" : 12000,
+    "stock" : True
+}
+
+#Accedemos al valor de la clave "precio"
+precio = producto["precio"]
+
+print(precio)
