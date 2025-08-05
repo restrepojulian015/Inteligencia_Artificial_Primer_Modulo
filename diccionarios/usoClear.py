3ecd9290ebea55e0a2530773588a1371af03ab4f
restrepojulian015 <restrepojulian015@gmail.com>
2025-08-05 12:37:41 -0500

    Uso de clear()

---
+++ diccionarios/usoClear.py
@@ -0,0 +1,11 @@
+#Uso del método clear()
+
+#Creación del diccionario
+carrito_compras = {"item1": 5, "item2": 2}
+
+print(carrito_compras)
+
+#uso de método clear()
+carrito_compras.clear()
+
+print(carrito_compras)
