1599980a20b86c96a3d133cae6fe9739b2c6c388
restrepojulian015 <restrepojulian015@gmail.com>
2025-08-05 15:47:52 -0500

    ultimos ejercicios

---
+++ diccionarios/iterarDiccionario.py
@@ -0,0 +1,9 @@
+#Iterar sobre un diccionario
+
+#Crear un diccionari
+from llavesValoresItems import capitales
+
+#iterar sobre el diccionario
+for pais, capital in capitales.items() :
+    print(f'La capital de {pais} es {capital}')
+
---
+++ diccionarios/llavesValoresItems.py
@@ -0,0 +1,13 @@
+#Uso de keys(), values() y items()
+
+#Creación del diccionario
+capitales = {"Colombia": "Bogotá", "Argentina": "Buenos Aires", "Perú": "Lima"}
+
+#Imprimir las llaves
+print(capitales.keys())
+
+#Imprimir los valores
+print(capitales.values())
+
+#Imprimir los items
+print(capitales.items())
